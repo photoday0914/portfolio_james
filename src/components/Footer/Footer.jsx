602955729f279ae58e7a3f3dc55828db6b333a20
Phillip Grandinetti <phillipgrandi89@gmail.com>
2023-08-09 13:39:41 -0400

    0809-1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className={s.footer}>
       <div className={s.container}>
         <div className={s.copyright}>
-          <h3>Copyright © {year} Phillip Grande</h3>
+          <h3>Copyright © 2023{year} James Brown</h3>
         </div>
       </div>
     </div>
