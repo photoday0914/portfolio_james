4b21cdb88533acf52a05a4e8b229562556a93339
Phillip Grandinetti <phillipgrandi89@gmail.com>
2023-08-09 13:48:49 -0400

    0809-2

--- src/scenes/Contact/Contact.jsx
+++ src/scenes/Contact/Contact.jsx
@@ -22,7 +22,7 @@ const Resume = () => {
             <br />
             <p className={s.smallText} style={{textAlign:"left"}}>PHONE NUMBER : +1 (805) 214-8119</p>
             <br />      
-            <p className={s.smallText} style={{textAlign:"left"}}>EMAIL ID : jameswn0710@gmail.com</p>
+            <p className={s.smallText} style={{textAlign:"left"}}>EMAIL ID : jamesbr0710@outlook.com</p>
             <br />
             <div>
               <p className={s.smallText} style={{textAlign:"left"}}>LOCATION : Remote</p>              
